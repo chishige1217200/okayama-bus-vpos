b80377a3d2cfd7eee330b536a5f641cb2088abe4
chishige1217200 <daichicchi01@gmail.com>
2024-11-17 23:40:00 +0900

    Add: Markerの表示処理
    Prob: 起動途中に追加しないと表示されない不具合あり

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
   height: "100vh",
@@ -7,18 +7,41 @@ const containerStyle = {
 };
 
 const center = {
-  lat: 34.66300000,
-  lng: 133.9250000,
+  lat: 34.663,
+  lng: 133.925,
+};
+
+const positionF1302 = {
+  lat: 34.51649856567383,
+  lng: 133.89849853515625,
+};
+
+const positionF1908 = {
+  lat: 34.62188720703125,
+  lng: 133.9432830810547,
 };
 
 const App = () => {
+  // TODO:ここにバス情報ロード処理が必要
+
   return (
     <LoadScript googleMapsApiKey="YOUR API KEY">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={17}
-      ></GoogleMap>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        <Marker
+          position={positionF1302}
+          icon={
+            "https://loc.bus-vision.jp/ryobi/view/images/common/busicon/10000/2/201_s.png"
+          }
+          label={"F1302"}
+        />
+        <Marker
+          position={positionF1908}
+          icon={
+            "https://loc.bus-vision.jp/ryobi/view/images/common/busicon/10000/2/209_s.png"
+          }
+          label={"F1908"}
+        />
+      </GoogleMap>
     </LoadScript>
   );
 };
