648a0e00ed7abbfae674c54b1b11b3d34497ae42
chishige1217200 <daichicchi01@gmail.com>
2024-11-18 23:05:19 +0900

    Fix: useEffectを用いた遅延描画でMarkerを表示するように修正

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -24,23 +24,38 @@ const positionF1908 = {
 const App = () => {
   // TODO:ここにバス情報ロード処理が必要
 
+  const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    // コンポーネントがマウントされた後に動的に内容を設定
+    const timer = setTimeout(() => {
+      setContent(
+        <>
+          <Marker
+            position={positionF1302}
+            icon={
+              "https://loc.bus-vision.jp/ryobi/view/images/common/busicon/10000/2/201_s.png"
+            }
+            label={"F1302"}
+          />
+          <Marker
+            position={positionF1908}
+            icon={
+              "https://loc.bus-vision.jp/ryobi/view/images/common/busicon/10000/2/209_s.png"
+            }
+            label={"F1908"}
+          />
+        </>
+      );
+    }, 100); // 0.1秒後にマーカー埋め込み（遅延描画しないとマーカーが表示されないため）
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <LoadScript googleMapsApiKey="YOUR API KEY">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-        <Marker
-          position={positionF1302}
-          icon={
-            "https://loc.bus-vision.jp/ryobi/view/images/common/busicon/10000/2/201_s.png"
-          }
-          label={"F1302"}
-        />
-        <Marker
-          position={positionF1908}
-          icon={
-            "https://loc.bus-vision.jp/ryobi/view/images/common/busicon/10000/2/209_s.png"
-          }
-          label={"F1908"}
-        />
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
+        {content}
       </GoogleMap>
     </LoadScript>
   );
