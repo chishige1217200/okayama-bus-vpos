4984f4b28d90a2cec463a35f34b64f7baf06687b
chishige1217200 <daichicchi01@gmail.com>
2025-03-15 18:28:39 +0900

    Fix: currentIndexが見つからない場合は、始発地点の遅れ時分を返す

--- src/App.js
+++ src/App.js
@@ -49,12 +49,8 @@ const App = () => {
       return Math.floor(stops[currentIndex].arrival.delay / 60) || null; // 正常に遅れ時分が取得できない場合はnull
     }
 
-    // 次のインデックスが存在する場合は、次の遅れ時分を返す
-    if (currentIndex !== -1 && currentIndex + 1 < stops.length) {
-      return Math.floor(stops[currentIndex + 1].arrival.delay / 60) || null; // 正常に遅れ時分が取得できない場合はnull
-    }
-
-    return null; // 正常に遅れ時分が取得できない場合はnull
+    // インデックスが取得できない場合は、始発時点の遅れ時分を返す
+    return Math.floor(stops[0].arrival.delay / 60) || null; // 正常に遅れ時分が取得できない場合はnull
   };
 
   const fetchMarkers = async () => {
